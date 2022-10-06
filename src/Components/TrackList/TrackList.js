3d6f228c2f76683ec51434d5321863023a49d168
Anna Clausen <anna.revall.clausen@gmail.com>
2022-10-06 14:28:56 -0700

    build components

---
+++ src/Components/TrackList/TrackList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './TrackList.css';
+
+class TrackList extends React.Component {
+    render() {
+        return (
+            <div className="TrackList">
+                <h1>Hello</h1>
+            </div>
+        )
+    }
+}
+
+export default TrackList;
